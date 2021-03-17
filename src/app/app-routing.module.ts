600cec690c31d8cd397600382fe29b7b6f1bdef7
Logiq01 <51047229+Logiq01@users.noreply.github.com>
2021-03-17 17:43:01 +0800

    Initial Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: LoginComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ]
+  },
+
+];
+
+
+
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
