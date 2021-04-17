600d47b2da5aa37ddb29cb82125e3cd3fd54a45c
Logiq01 <51047229+Logiq01@users.noreply.github.com>
2021-04-18 05:42:26 +0800

    added functionality for sidebar and fixed calendar bug

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -31,22 +31,6 @@ export interface ChildrenItems {
 export class SidebarComponent implements OnInit {
     @Output() public pageTitle: EventEmitter<string> = new EventEmitter<string>();
     public path: string;
-    public menuItems: any[];
-    ps: any;
-    public onClick = false;
-    public activeDashboard: Boolean = true;
-    public roles: Array<string>;
-    public showBookingArrow: Boolean = true;
-    public showTripArrow: Boolean = true;
-    public showAccountingArrow: Boolean = true;
-    public showDocumentsArrow: Boolean = true;
-    public showInventoryArrow: Boolean = true;
-    public showRepairArrow: Boolean = true;
-    public showUserArrow: Boolean = true;
-    public showVehicleArrow: Boolean = true;
-    public isLoggedIn: boolean;
-    isAdmin: boolean;
-
     constructor(
         private router: Router,
         public route: ActivatedRoute,
@@ -69,11 +53,7 @@ export class SidebarComponent implements OnInit {
     ngAfterViewInit() {
     }
 
-    updatePS(): void {
-        if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
-            this.ps.update();
-        }
-    }
+
 
     isMac(): boolean {
         let bool = false;
@@ -91,96 +71,20 @@ export class SidebarComponent implements OnInit {
     };
 
 
-    goToCreateBooking() {
-        this.router.navigate(['/booking/create-booking']);
-        this.path = '/booking/create-booking'
-    }
-    goToListOfBooking() {
-        this.router.navigate(['/booking/list-of-booking']);
-        this.path = '/booking/list-of-booking';
 
-    }
     goToDashboard() {
-        this.router.navigate(['dashboard']);
-        this.path = '/dashboard';
+        this.router.navigate(['/dashboard/dashboard']);
     }
-    goToCashVoucher() {
-        this.router.navigate(['/cash-advance/cash-advance']);
-        this.path = '/cash-advance/cash-advance';
+    goToKanbanBoard() {
+        this.router.navigate(['/kanban-board/kanban-board']);
     }
-    goToListOfVoucher() {
-        this.router.navigate(['/cash-advance/cash-count']);
-        this.path = '/cash-advance/cash-count';
+    goToCalendar() {
+        this.router.navigate(['/calendar/calendar']);
+    }
+    goToRoadmap() {
+        this.router.navigate(['/roadmap/roadmap']);
     }
 
-    goToManageTrip() {
-        this.router.navigate(['/trip/manage-trip']);
-        this.path = '/trip/manage-trip';
-    }
-    goToListOfTrip() {
-        this.router.navigateByUrl('/trip/list-of-trip');
-        this.path = '/trip/list-of-trip';
-    }
-    goToListOfPayroll() {
-        this.router.navigateByUrl('/payroll/list-of-payroll');
-        this.path = '/payroll/list-of-payroll';
-    }
-    goToFileUpload() {
-        this.router.navigate(['/file-upload']);
-        this.path = '/file-upload';
-    }
-    goToTransmittal() {
-        this.router.navigate(['/transmittal']);
-        this.path = '/transmittal';
-    }
-    goToAccessLevel() {
-        this.router.navigate(['/access-level/access-level']);
-    }
-    goToAddEmployee() {
-        this.router.navigate(['/add-employee/add-employee']);
-        this.path = '/add-employee/add-employee';
-
-    }
-    goToAddClient() {
-        this.router.navigate(['/add-client/add-client']);
-        this.path = '/add-client/add-client';
-    }
-    goToAddTruck() {
-        this.router.navigate(['/truck/add-truck']);
-        this.path = '/truck/add-truck';
-    }
-    goToListOfEmployee() {
-        this.router.navigate(['/list-of-employee/list-of-employee']);
-        this.path = '/list-of-employee/list-of-employee';
-    }
-    goToListOfClient() {
-        this.router.navigate(['/list-of-client/list-of-client']);
-        this.path = '/list-of-client/list-of-client';
-    }
-    goToListOfTruck() {
-        this.router.navigate(['/truck/list-of-truck']);
-        this.path = '/truck/list-of-truck';
-    }
-    goToRepairs() {
-        this.router.navigate(['/repairs/repairs']);
-        this.path = '/repairs/repairs';
-    }
-    goToListOfRepairs() {
-        this.router.navigate(['/list-of-repairs/list-of-repairs']);
-        this.path = '/list-of-repairs/list-of-repairs';
-    }
-    goToInventory() {
-        this.router.navigate(['/inventory/inventory']);
-        this.path = '/inventory/inventory';
-    }
-    goToListOfInventory() {
-        this.router.navigate(['/list-of-inventory/list-of-inventory']);
-        this.path = '/list-of-inventory/list-of-inventory';
-    }
-    goToAccountingListOfBooking() {
-        this.router.navigate(['/accounting-list-of-booking/accounting-list-of-booking']);
-        this.path = '/accounting-list-of-booking/accounting-list-of-booking';
-    }
 
 
     setPageTitle(page: string) {
