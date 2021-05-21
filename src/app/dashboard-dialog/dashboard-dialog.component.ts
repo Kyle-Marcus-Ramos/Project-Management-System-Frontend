4087238148e74d1ec641cff09dfa45cf7050f9b5
Neilz05 <neil_dong@dlsu.edu.ph>
2021-05-21 23:42:01 +0800

    added dashboard modal

--- src/app/dashboard-dialog/dashboard-dialog.component.ts
+++ src/app/dashboard-dialog/dashboard-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-dashboard-dialog',
   templateUrl: './dashboard-dialog.component.html',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardDialogComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private fb:FormBuilder) { }
+  productForm: FormGroup;
   ngOnInit(): void {
+    this.productForm = this.fb.group({
+      title: [],
+      collaborator: this.fb.array([this.fb.group({point:''})])})
   }
 
+  get collaborator() {
+    return this.productForm.get('collaborator') as FormArray;
+  }
+
+  addCollaborator() {
+    this.collaborator.push(this.fb.group({point:''}));
+  }
+
+  deleteCollaborator(index) {
+    this.collaborator.removeAt(index);
+  }
+
+  name: string
+  collaborators: Collaborator[]
+}
+export class Collaborator{
+  collaborators: string
 }
